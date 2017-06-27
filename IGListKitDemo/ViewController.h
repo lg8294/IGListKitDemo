75dc1aa6eb5843f53ce0cf07fc9256351995617a
arrcen-lg <515930926@qq.com>
2017-06-27 16:27:00 +0800

    Initial Commit

---
+++ IGListKitDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IGListKitDemo
+//
+//  Created by iOS on 2017/6/27.
+//  Copyright © 2017年 lg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ IGListKitDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  IGListKitDemo
+//
+//  Created by iOS on 2017/6/27.
+//  Copyright © 2017年 lg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
